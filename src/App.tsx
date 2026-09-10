65f7cb831edba9e8b2bad3d8c15dfda1ae2ba340
ReiVon <nickfaqs20@gmail.com>
2026-09-10 17:16:41 -0500

    chore: initial React + TypeScript + PrimeReact project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'primereact/button'
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Prueba de PrimeReact</h1>
+      <Button label="Funciona 🎉" icon="pi pi-check" />
+    </div>
+  )
+}
+
+export default App
